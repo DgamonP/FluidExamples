27ec94ee7d4b82812ce0c5ece69f5a03464c840b
David <dgamonp@gmail.com>
2020-09-24 16:36:12 -0400

    change port ip

--- draft-js/jest.config.js
+++ draft-js/jest.config.js
@@ -13,7 +13,7 @@ process.env["PORT"] = mappedPort;
 module.exports = {
   preset: "jest-puppeteer",
   globals: {
-    PATH: `http://localhost:${mappedPort}`
+    PATH: `http://0.0.0.0:${mappedPort}`
   },
   testMatch: ["**/?(*.)+(spec|test).[t]s"],
   testPathIgnorePatterns: ['/node_modules/', 'dist'],
